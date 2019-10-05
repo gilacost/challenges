3f798071fcea574999be5382cb09aab8397455e5
gilacost <josep.giralt@derivco.se>
2019-10-05 16:45:29 +0200

    fix: implement expected behaviour

--- lib/path.ex
+++ lib/path.ex
@@ -1,12 +1,12 @@
 defmodule Challenges.Path do
   @moduledoc """
   This module defines the functions to find a path in a 5x5 matrix.
-  Expects an string that might have the next symbols: [u,d,r,l,?].
+  Expects an string that might have the next characters: [u,d,r,l,?].
   The letters stand for up, down, right and left.
 
   The question marks are directions in the coordinates that need to
-  be found taking into account that no the path needs to be unique.
-  and that possitions veisted previously might not be visited again,
+  be found taking into account that the path needs to be unique and that
+  should never go outside the 5X5 matrix.
   """
 
   @doc """
@@ -17,6 +17,7 @@ defmodule Challenges.Path do
 
       iex> Challenges.Path.correct("drdr??rrddd?")
       "drdruurrdddd"
+
 
   ## First example matrix:
 
@@ -33,33 +34,63 @@ defmodule Challenges.Path do
       0 r u 0 d
       0 0 0 0 d
       0 0 0 0 0
+
   """
-  def correct(string) do
-    string
+  def correct(inputString) do
+    inputString
     |> String.graphemes()
-    |> directionsCount()
-    |> pendingDriections()
+    |> Enum.map(&randomDirection(&1))
+    |> Enum.join()
+    |> checkPath(inputString)
   end
 
-  def directionsCount(pathList) do
-    directions = %{
-      "d" => 0,
-      "u" => 0,
-      "l" => 0,
-      "r" => 0,
-      "?" => 0
-    }
+  def checkPath(testString, inputString) do
+    {history, last_position} =
+      testString
+      |> String.graphemes()
+      |> Enum.reduce_while({{}, [0, 0]}, fn d, {history, acc} ->
+        [x, y] = forward(d, acc)
 
-    pathList
-    |> Enum.map_reduce(directions, fn d, acc ->
-      acc = Map.update!(acc, "#{d}", &(&1 + 1))
-      {pathList, acc}
-    end)
+        acc = {
+          Tuple.append(history, [x, y]),
+          [x, y]
+        }
+
+        if x < 0 or x > 4 or y > 0 or y < -4 do
+          {:halt, acc}
+        else
+          {:cont, acc}
+        end
+      end)
+
+    history_list = Tuple.to_list(history)
+    history_unique = Enum.uniq(history_list)
+
+    if length(history_list) == length(history_unique) and last_position == [4, -4] do
+      testString
+    else
+      correct(inputString)
+    end
   end
 
-  def pendingDriections({pathList, %{"d" => d, "u" => u, "l" => l, "r" => r, "?" => unk}}) do
-    IO.inspect(pathList)
-    _currentPosition = {u - d, r - l} |> IO.inspect()
-    IO.inspect(unk)
+  def randomDirection("?"),
+    do:
+      Enum.random([
+        "d",
+        "r",
+        "u",
+        "l"
+      ])
+
+  def randomDirection(d), do: d
+
+  def forward(direction, [x, y]) do
+    case direction do
+      "d" -> [x, y - 1]
+      "r" -> [x + 1, y]
+      "u" -> [x, y + 1]
+      "l" -> [x - 1, y]
+      _ -> 0
+    end
   end
 end
